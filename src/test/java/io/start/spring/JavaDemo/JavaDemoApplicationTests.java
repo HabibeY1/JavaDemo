bcd100e8f2aa5230bc950dff610ac442704fa31e
Khatai Alizade <qaqaw4930@gmail.com>
2024-06-06 14:36:03 +0200

    message

---
+++ src/test/java/io/start/spring/JavaDemo/JavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.start.spring.JavaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
